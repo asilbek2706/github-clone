138a36d163822c365eea02f55129c69b60083ff4
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-08 20:31:41 +0500

    feat: configure cookie parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import helmet from 'helmet';
 
@@ -18,6 +19,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/api/health', (_req, res) => {
   res.status(200).json({
